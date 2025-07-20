2ac6dfc574516dadccdfdcfdce4e10cd10f3f126
saturngod <saturngod@gmail.com>
2025-07-20 13:23:59 -0500

    demo deploy

--- database/seeders/UnitsSeeder.php
+++ database/seeders/UnitsSeeder.php
@@ -13,43 +13,43 @@ class UnitsSeeder extends Seeder
     {
         $units = [
             // Mass
-            ['name' => 'Gram', 'type' => 'mass'],
-            ['name' => 'Kilogram', 'type' => 'mass'],
-            ['name' => 'Viss', 'type' => 'mass'],
-            ['name' => 'Tical', 'type' => 'mass'],
-            ['name' => 'Pinch', 'type' => 'mass'],
+            ['name' => 'Gram', 'unit_type' => 'mass'],
+            ['name' => 'Kilogram', 'unit_type' => 'mass'],
+            ['name' => 'Viss', 'unit_type' => 'mass'],
+            ['name' => 'Tical', 'unit_type' => 'mass'],
+            ['name' => 'Pinch', 'unit_type' => 'mass'],
 
             // Volume
-            ['name' => 'Milliliter', 'type' => 'volume'],
-            ['name' => 'Liter', 'type' => 'volume'],
-            ['name' => 'Tablespoon', 'type' => 'volume'],
-            ['name' => 'Teaspoon', 'type' => 'volume'],
-            ['name' => 'Cup', 'type' => 'volume'],
-            ['name' => 'Spoon', 'type' => 'volume'],
+            ['name' => 'Milliliter', 'unit_type' => 'volume'],
+            ['name' => 'Liter', 'unit_type' => 'volume'],
+            ['name' => 'Tablespoon', 'unit_type' => 'volume'],
+            ['name' => 'Teaspoon', 'unit_type' => 'volume'],
+            ['name' => 'Cup', 'unit_type' => 'volume'],
+            ['name' => 'Spoon', 'unit_type' => 'volume'],
 
             // Quantity
-            ['name' => 'Piece', 'type' => 'quantity'],
-            ['name' => 'Bunch', 'type' => 'quantity'],
-            ['name' => 'Round', 'type' => 'quantity'],
-            ['name' => 'Clove', 'type' => 'quantity'],
-            ['name' => 'Clove (burmese)', 'type' => 'quantity'],
-            ['name' => 'Sprig', 'type' => 'quantity'],
-            ['name' => 'Head', 'type' => 'quantity'],
-            ['name' => 'Bulb', 'type' => 'quantity'],
-            ['name' => 'Slice', 'type' => 'quantity'],
-            ['name' => 'Inch', 'type' => 'quantity'],
-            ['name' => 'Can', 'type' => 'quantity'],
-            ['name' => 'Stalk', 'type' => 'quantity'],
-            ['name' => 'Leaf', 'type' => 'quantity'],
-            ['name' => 'Pod', 'type' => 'quantity'],
-            ['name' => 'Ear', 'type' => 'quantity'],
-            ['name' => 'Pack', 'type' => 'quantity'],
-            ['name' => 'Whole', 'type' => 'quantity'],
-            ['name' => 'Stick', 'type' => 'quantity'],
+            ['name' => 'Piece', 'unit_type' => 'quantity'],
+            ['name' => 'Bunch', 'unit_type' => 'quantity'],
+            ['name' => 'Round', 'unit_type' => 'quantity'],
+            ['name' => 'Clove', 'unit_type' => 'quantity'],
+            ['name' => 'Clove (burmese)', 'unit_type' => 'quantity'],
+            ['name' => 'Sprig', 'unit_type' => 'quantity'],
+            ['name' => 'Head', 'unit_type' => 'quantity'],
+            ['name' => 'Bulb', 'unit_type' => 'quantity'],
+            ['name' => 'Slice', 'unit_type' => 'quantity'],
+            ['name' => 'Inch', 'unit_type' => 'quantity'],
+            ['name' => 'Can', 'unit_type' => 'quantity'],
+            ['name' => 'Stalk', 'unit_type' => 'quantity'],
+            ['name' => 'Leaf', 'unit_type' => 'quantity'],
+            ['name' => 'Pod', 'unit_type' => 'quantity'],
+            ['name' => 'Ear', 'unit_type' => 'quantity'],
+            ['name' => 'Pack', 'unit_type' => 'quantity'],
+            ['name' => 'Whole', 'unit_type' => 'quantity'],
+            ['name' => 'Stick', 'unit_type' => 'quantity'],
         ];
 
         foreach ($units as $unit) {
-            Unit::firstOrCreate($unit);
+            Unit::firstOrCreate(['name' => $unit['name']], $unit);
         }
     }
 }
